0b2b67aa15a350dba57b59290bfcdca8337ce110
marzeverett <marzeverett@gmail.com>
2022-12-07 20:08:20 -0800

    A try

---
+++ servant.py
@@ -0,0 +1,18 @@
+
+import node_classes as nc
+import json
+
+connection_info = {
+    #"mqtt_username":,
+     #"mqtt_password":, 
+     "mqtt_url": "192.168.0.28", 
+     "mqtt_port": 1883
+
+}
+
+
+message = {"Hola": "Como estas?"}
+servant = nc.servant("servant", connection_info, "Hola")
+servant.disconnect()
+
+
