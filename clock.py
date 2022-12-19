931ae493c094d23b2a838fd6830eaf9bca21d344
marzeverett <marzeverett@gmail.com>
2022-12-19 14:52:55 -0800

    A clock again

--- clock.py
+++ clock.py
@@ -1,21 +1,53 @@
 
 
-#name associated with device ID and action
-import time
+
+#Almost Direct copy from here: https://keithweaverca.medium.com/controlling-stepper-motors-using-python-with-a-raspberry-pi-b3fbd482f886
+
 import RPi.GPIO as GPIO
-from RpiMotorLib import RpiMotorLib
-
-#clock_pins = [17, 18, 27, 22]
-clock_pins = [7, 11, 15, 13]
-clock_motor = RpiMotorLib.BYJMotor("StepperMotor", "28BYJ")
-
-
-sleep_time = 1.5
+import time
+GPIO.setmode(GPIO.BOARD)
+control_pins = [7,11,15,13]
 
 def action(message):
-    clock_motor.motor_run(clock_pins, .003, 500, False, False, "wave", .05) #.005 $
-    time.sleep(4)
+    for pin in control_pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)halfstep_seq = [
+    [1,0,0,0],
+    [1,1,0,0],
+    [0,1,0,0],
+    [0,1,1,0],
+    [0,0,1,0],
+    [0,0,1,1],
+    [0,0,0,1],
+    [1,0,0,1]
+    ]for i in range(512):
+    for halfstep in range(8):
+        for pin in range(4):
+        GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+        time.sleep(0.001)
+        
     GPIO.cleanup()
 
+action()
 
-action("hi")
+
+
+# #name associated with device ID and action
+# import time
+# import RPi.GPIO as GPIO
+# from RpiMotorLib import RpiMotorLib
+
+# #clock_pins = [17, 18, 27, 22]
+# clock_pins = [7, 11, 15, 13]
+# clock_motor = RpiMotorLib.BYJMotor("StepperMotor", "28BYJ")
+
+
+# sleep_time = 1.5
+
+# def action(message):
+#     clock_motor.motor_run(clock_pins, .003, 500, False, False, "wave", .05) #.005 $
+#     time.sleep(4)
+#     GPIO.cleanup()
+
+
+# action("hi")
