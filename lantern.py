6c4b4d8c8366c5630d22a743126b8682aaa97173
marzeverett <marzeverett@gmail.com>
2022-12-19 13:54:20 -0800

    One more time

--- lantern.py
+++ lantern.py
@@ -3,13 +3,14 @@ import time
 
 
 def write_read(x):
+    arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1)
     arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
     data = arduino.readline()
     return data
 
 def action(message):
-    arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1)
+    
 
     print("Before serial send")
     arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1)
