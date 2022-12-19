d95af2a46e6da7ec4ac3ffe7cd9158f4625ae021
marzeverett <marzeverett@gmail.com>
2022-12-19 14:54:08 -0800

    more

--- clock.py
+++ clock.py
@@ -11,7 +11,8 @@ control_pins = [7,11,15,13]
 def action(message):
     for pin in control_pins:
         GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, 0)halfstep_seq = [
+        GPIO.output(pin, 0)
+        halfstep_seq = [
         [1,0,0,0],
         [1,1,0,0],
         [0,1,0,0],
@@ -20,11 +21,12 @@ def action(message):
         [0,0,1,1],
         [0,0,0,1],
         [1,0,0,1]
-        ]for i in range(512):
-        for halfstep in range(8):
-            for pin in range(4):
-            GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-            time.sleep(0.001)
+        ]
+        for i in range(512):
+            for halfstep in range(8):
+                for pin in range(4):
+                GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+                time.sleep(0.001)
     GPIO.cleanup()
 
 action()
