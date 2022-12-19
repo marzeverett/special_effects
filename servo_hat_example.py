d87a4a4a9125c9fb0a36a58f56d26832d84d1176
marzeverett <marzeverett@gmail.com>
2022-12-19 12:21:43 -0800

    More servo nonsense

--- servo_hat_example.py
+++ servo_hat_example.py
@@ -10,18 +10,11 @@ print("start")
 pwm = PCA9685(0x40)
 pwm.setPWMFreq(50)
 
-Pos0 = 1500  
-Pos1 = 1500 
-Pos2 = 1500 
-Pos3 = 1500 
-Step0 = 0  
-Step1 = 0  
-Step2 = 0  
-Step3 = 0  
-pwm.setServoPulse(0,1500)
-time.sleep(5)
-pwm.setServoPulse(0,500)
-time.sleep(5)
+for i in range(0, 4):
+	pwm.setServoPulse(0,2100)
+	time.sleep(1)
+	pwm.setServoPulse(0,500)
+	time.sleep(1)
 #pwm.setServoPulse(1,Pos1)
 #pwm.setServoPulse(2,Pos2)
 #pwm.setServoPulse(3,Pos3)
