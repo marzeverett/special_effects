8e5b2e1b0375b82bd301bac15d2505d61ac7d596
marzeverett <marzeverett@gmail.com>
2022-12-19 14:55:39 -0800

    more

--- clock.py
+++ clock.py
@@ -7,9 +7,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 control_pins = [7,11,15,13]
-
-def action(message):
-    for pin in control_pins:
+for pin in control_pins:
         GPIO.setup(pin, GPIO.OUT)
         GPIO.output(pin, 0)
         halfstep_seq = [
@@ -22,6 +20,9 @@ def action(message):
         [0,0,0,1],
         [1,0,0,1]
         ]
+
+def action(message):
+    
         for i in range(512):
             for halfstep in range(8):
                 for pin in range(4):
