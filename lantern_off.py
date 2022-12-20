df208d9bef5468cf8c354258b38e69cae57ed549
marzeverett <marzeverett@gmail.com>
2022-12-19 16:28:45 -0800

    more changes to structure

---
+++ lantern_off.py
@@ -0,0 +1,21 @@
+import serial
+import time
+
+arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1)
+
+
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
+
+def action(message):
+    print("Before serial send")
+    print("After serial send")
+    num = str(4)
+    value = write_read(num)
+    print(value) # printing the value
+
+
+    
